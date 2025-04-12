4a29759a2862a38705406cee77caacd5606a8c2d
Mickael Pires <83048320+mickpires@users.noreply.github.com>
2025-04-12 14:19:34 -0300

    deu ruim até agora. reescrevi o trapezoidal, mas tenho que melhorar

--- trapezoidal.py
+++ trapezoidal.py
@@ -1,5 +1,5 @@
 import jax.numpy as np
-from jax import grad
+from jax import jacobian
 
 
 # trapezoidal usando ponto fixo
@@ -16,15 +16,18 @@ from jax import grad
 #                 guest = new_guest
 #     return y
 
-def trapezoidal(F, y: np.ndarray, t: np.ndarray, k: float, tol: float):
-    for i in range(len(y) - 1):
-        G = lambda x: (x - y[i]) / k - (F(t[i+1], x) + F(t[i], y[i])) / 2
-        dG = grad(G)
-        guest = y[i]
+def trapezoidal(F, y0: np.ndarray,pontos:int, t: np.ndarray, k: float, tol: float):
+    num_equations = len(y0)
+    y = np.empty((len(y0),pontos),dtype=float)
+    y = y.at[:,0].set(y0[:])
+    for i in range(pontos - 1):
+        G = lambda x: np.array([(x[j] - y[j,i]) / k - (F[j](t[i+1], x[j]) + F[j](t[i], y[j,i])) / 2 for j in range(num_equations)]).T
+        dG = jacobian(G)
+        guest = y[:,i]
         while True:
-            new_guest = guest - G(guest) / dG(guest)
-            if np.abs(new_guest - guest) <= tol:
-                y[i+1] = new_guest
+            new_guest = guest - np.linalg.inv(dG(guest))@G(guest)
+            if np.abs(new_guest[0] - guest[0]) <= tol:
+                y = y.at[:,i+1].set(new_guest)
                 break
             else:
                 guest = new_guest
